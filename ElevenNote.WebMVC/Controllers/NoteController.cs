e01480ede596f40e110e6db270ac999015b4691b
zjmcdon98 <zjmcdon98@gmail.com>
2018-11-25 13:55:52 -0500

    Edit

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ElevenNote.WebMVC/Models/NoteListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.WebMVC.Models
+{
+    public class NoteListItem
+    {
+        public int NotedId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="created")]
+        public DateTimeOffset CreateUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
