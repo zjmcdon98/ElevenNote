6e5ed9058c2663c70c9dd8582da0573eab9d7941
zjmcdon98 <zjmcdon98@gmail.com>
2018-11-25 14:11:48 -0500

    Edit

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -16,5 +16,11 @@ namespace ElevenNote.WebMVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        //Get
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
--- ElevenNote.WebMVC/Models/NoteCreate.cs
+++ ElevenNote.WebMVC/Models/NoteCreate.cs
@@ -6,13 +6,15 @@ using System.Web;
 
 namespace ElevenNote.WebMVC.Models
 {
-    public class NoteListItem
+    public class NoteCreate
     {
-        public int NotedId { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "Please Enter At Least Two Characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field")]
         public string Title { get; set; }
 
-        [Display(Name="created")]
-        public DateTimeOffset CreateUtc { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
 
         public override string ToString() => Title;
         {
